c7d69d7821139a82925a90d86a0150cf89b5f6c4
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-08 21:55:00 +0100

    chore(hooks): set up git hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,30 @@ module.exports = {
     project: './tsconfig.json',
     createDefaultProgram: true,
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
   },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended' // Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    'no-eval': 2,
+    '@typescript-eslint/no-shadow': 2,
+    //ignored
+    'no-shadow': 0,
     '@typescript-eslint/no-namespace': 0,
     '@typescript-eslint/restrict-template-expressions': 0
   }
